89a83136c6a46bb7ef11d1743e4c66d979780fb0
stan <sidelnikov.stanislav@gmail.com>
2013-08-15 20:38:08 +0600

    Initial app with a simple action bar and fixed tabs. Android APIs 8 and above.

---
+++ ActionBarSample/src/ru/glossit/actionbarsample/MainActivity.java
@@ -0,0 +1,98 @@
+package ru.glossit.actionbarsample;
+
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBar.Tab;
+import android.support.v7.app.ActionBarActivity;
+//import android.support.v7.appcompat.R.layout;
+import android.view.Menu;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        
+        // Notice that setContentView() is not used, because we use the root
+        // android.R.id.content as the container for each fragment
+
+        // setup action bar for tabs
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        //actionBar.setDisplayShowTitleEnabled(false);
+
+        Tab tab = actionBar.newTab()
+                           .setText("Artist")
+                           .setTabListener(new TabListener<ArtistFragment>(
+                                   this, "artist", ArtistFragment.class));
+        actionBar.addTab(tab);
+
+        tab = actionBar.newTab()
+                       .setText("Album")
+                       .setTabListener(new TabListener<AlbumFragment>(
+                               this, "album", AlbumFragment.class));
+        actionBar.addTab(tab);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    public static class TabListener<T extends Fragment> implements ActionBar.TabListener {
+	    private Fragment mFragment;
+	    private final Activity mActivity;
+	    private final String mTag;
+	    private final Class<T> mClass;
+	
+	    /** Constructor used each time a new tab is created.
+	      * @param activity  The host Activity, used to instantiate the fragment
+	      * @param tag  The identifier tag for the fragment
+	      * @param clz  The fragment's Class, used to instantiate the fragment
+	      */
+	    public TabListener(Activity activity, String tag, Class<T> clz) {
+	        mActivity = activity;
+	        mTag = tag;
+	        mClass = clz;
+	    }
+	
+	    /* The following are each of the ActionBar.TabListener callbacks */
+	
+	    public void onTabSelected(Tab tab, FragmentTransaction ft) {
+	        // Check if the fragment is already initialized
+	        if (mFragment == null) {
+	            // If not, instantiate and add it to the activity
+	            mFragment = Fragment.instantiate(mActivity, mClass.getName());
+	            ft.add(getContentViewCompat(), mFragment, mTag);
+	        } else {
+	            // If it exists, simply attach it in order to show it
+	            ft.attach(mFragment);
+	        }
+	    }
+	    
+	    public static int getContentViewCompat() {
+	        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH ?
+	                   android.R.id.content : R.id.action_bar_activity_content;
+	    }
+	
+	    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+	        if (mFragment != null) {
+	            // Detach the fragment, because another one is being attached
+	            ft.detach(mFragment);
+	        }
+	    }
+	
+	    public void onTabReselected(Tab tab, FragmentTransaction ft) {
+	        // User selected the already selected tab. Usually do nothing.
+	    }
+	}
+}
+
+
